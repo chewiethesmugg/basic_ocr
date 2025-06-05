519500025c6898bd61f7b54a8f53868dc38a45a7
chewiethesmugg <chewantha@gmail.com>
2025-06-05 19:24:23 -0400

    more changes

--- main.py
+++ main.py
@@ -15,8 +15,30 @@ def show_image(file):
 
 #inverting image
 
-img=cv2.imread(file_name)
-inverted_image = cv2.bitwise_not(img)
-cv2.imwrite("temp/inverted_"+image_name,inverted_image)
-print("done")
-show_image("temp/inverted_"+image_name)
+def invert_image(image_file):
+    img=cv2.imread(file_name)
+    inverted_image = cv2.bitwise_not(img)
+    inv_name= "temp/inverted_"+image_name
+    cv2.imwrite(inv_name,inverted_image)
+    return inv_name
+
+invertedImage = invert_image(file_name)
+#show_image(invertedImage)
+
+def greyscale(image_file):
+    img = cv2.imread(image_file)
+    return cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+gray_image = greyscale(file_name)
+gray_name= "temp/gray_"+image_name
+cv2.imwrite(gray_name, gray_image)
+
+
+#binarizing the image
+#we need to greyscale it first
+
+thresh, im_bw = cv2.threshold(gray_image, 50, 100, cv2.THRESH_BINARY)
+bin_name="temp/bin_"+image_name
+print(bin_name)
+cv2.imwrite(bin_name,im_bw)
+show_image(bin_name)
