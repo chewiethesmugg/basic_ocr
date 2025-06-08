646a4f8ca06408112287a072a25fcbdeae3b253d
chewiethesmugg <chewantha@gmail.com>
2025-06-08 19:16:31 -0400

    refactoring code

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 import cv2
 from PIL import Image
 import pytesseract
+import os
 
+# image functions
 
-# opening and showing inage
-image_name="IMG_1047.jpg"
-file_name = "test_images/"+image_name
-
-print(file_name)
-
+# display function
 def show_image(file):
     im = Image.open(file)
     print(im.show())
 
 #inverting image
-
 def invert_image(image_file):
     img=cv2.imread(file_name)
     inverted_image = cv2.bitwise_not(img)
@@ -22,29 +18,13 @@ def invert_image(image_file):
     cv2.imwrite(inv_name,inverted_image)
     return inv_name
 
-invertedImage = invert_image(file_name)
-#show_image(invertedImage)
-
+#greyscaling image
 def greyscale(image_file):
     img = cv2.imread(image_file)
     return cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-gray_image = greyscale(file_name)
-gray_name= "temp/gray_"+image_name
-cv2.imwrite(gray_name, gray_image)
-
-
-#binarizing the image
-#we need to greyscale it first
-
-thresh, im_bw = cv2.threshold(gray_image, 86, 500, cv2.THRESH_BINARY)
-bin_name="temp/bin_"+image_name
-print(bin_name)
-cv2.imwrite(bin_name,im_bw)
-#show_image(bin_name)
-
-#NOISE REMOVAL
-
+#noise removal
+#greyscale image first before input
 def noise_rem(image_file):
     import numpy as np
     #the kernal for erosion
@@ -55,10 +35,44 @@ def noise_rem(image_file):
     image = cv2.erode(image, kernal, iterations = 1)
     image = cv2.morphologyEx(image_file, cv2.MORPH_CLOSE, kernal)
     image = cv2.medianBlur(image, 3)
-
     return (image)
 
-print(bin_name)
-no_noise = noise_rem(bin_name)
-cv2.imwrite("temp/no_noise_"+image_name,no_noise)
-show_image("temp/no_noise_"+image_name)
+
+
+# loop through input images
+pwd_path = "C:\Users\user1\Desktop\PROJECTS\text_puller"
+pwd = os.fsencode(pwd_path)
+
+for imageFile in os.listdir(pwd):
+    imageFileName = os.fsdecode(imageFile)
+
+    outputpath = "test_images/"
+
+    invertedImage = invert_image(imageFileName)
+    cv2.imwrite(outputpath+"_inverted_"+imageFileName)
+
+
+
+
+#gray_image = greyscale(file_name)
+#gray_name= "temp/gray_"+image_name
+#cv2.imwrite(gray_name, gray_image)
+
+
+#binarizing the image
+#we need to greyscale it first
+
+#thresh, im_bw = cv2.threshold(gray_image, 86, 500, cv2.THRESH_BINARY)
+#bin_name="temp/bin_"+image_name
+#print(bin_name)
+#cv2.imwrite(bin_name,im_bw)
+#show_image(bin_name)
+
+#NOISE REMOVAL
+
+
+
+#print(bin_name)
+#no_noise = noise_rem(bin_name)
+#cv2.imwrite("temp/no_noise_"+image_name,no_noise)
+#show_image("temp/no_noise_"+image_name)
